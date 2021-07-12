cfa6972d480049e51b2fc99ed7801a2546604c04
Amanda Miquilini <amiquilini@id.uff.br>
2021-07-12 10:04:57 -0300

    Testes iniciais com movimentos possiveis das pecas

--- Program.cs
+++ Program.cs
@@ -18,10 +18,15 @@ namespace jogo_xadrez_console
                     Tela.ImprimirTabuleiro(partida.tab);
 
                     Console.WriteLine();
-
                     Console.Write("Origem: ");
                     Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
 
+                    bool[,] posicoesPossiveis = partida.tab.Peca(origem).MovimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
 
