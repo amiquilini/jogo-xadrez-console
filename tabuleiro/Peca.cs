f67fe05b5cda24c688efe490ef96905901ccb545
Amanda Miquilini <amiquilini@id.uff.br>
2021-07-12 13:44:21 -0300

    Mudando o nome do metodo para MovimentoPossivel

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -42,7 +42,7 @@ namespace tabuleiro
             return false;
         }
 
-        public bool PodeMoverPara(Posicao pos) {
+        public bool MovimentoPossivel(Posicao pos) {
             return MovimentosPossiveis()[pos.linha, pos.coluna];
         }
         public abstract bool[,] MovimentosPossiveis();
